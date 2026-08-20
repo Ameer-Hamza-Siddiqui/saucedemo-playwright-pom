8b3a487f29de3d7625283926b365a1b701e97efe
Ameer Hamza <ameerhamzasiddiqui4@gmai>
2026-08-21 02:19:43 +0500

    Add screenshots to login page methods

--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -1,3 +1,5 @@
+import {test} from '@playwright/test'
+
 class LoginPage {
   constructor(page) {
     this.page = page;
@@ -21,6 +23,29 @@ class LoginPage {
   async getErrorMessage() {
     return await this.errorMessage.textContent();
   }
-}
 
+  async attachScreenshot(name){
+  await test.info().attach(name,{
+    body: await this.page.screenshot(),
+    contentType:"image/png",
+  });
+  }
+
+   async gotoLoginPage(){
+    await this.page.goto('https://www.saucedemo.com/');
+    await this.attachScreenshot("01 login page opened")
+  }
+
+  async login(usernameInput,passwordInput){
+    await this.usernameInput.fill(usernameInput),
+    await this.attachScreenshot("02 After entering username ")
+
+    await this.passwordInput.fill(passwordInput),
+    await this.attachScreenshot("03 After entering password")
+
+    await this.loginButton.click(),
+    await this.attachScreenshot("04 After clicking login")
+  }
+}
+ 
 export default LoginPage;
