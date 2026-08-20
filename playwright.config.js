0595ef5db44a85cdd03a4e3c450f4daca5398805
Ameer Hamza <ameerhamzasiddiqui4@gmai>
2026-08-20 20:00:50 +0500

    Update Playwright tests

--- playwright.config.js
+++ playwright.config.js
@@ -23,7 +23,10 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter:[
+    ['list'],
+    ['allure-playwright']
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
@@ -77,5 +80,6 @@ export default defineConfig({
   //   url: 'http://localhost:3000',
   //   reuseExistingServer: !process.env.CI,
   // },
+ 
 });
 
